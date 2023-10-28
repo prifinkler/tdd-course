2e99112b9dcaf3733642685d39f05b948247cb5e
Priscila Finkler <prifinkler@gmail.com>
2023-10-28 22:46:45 +0100

    palindrome substring and slice

--- tests/word/word_spec.rb
+++ tests/word/word_spec.rb
@@ -4,7 +4,7 @@ describe Word do
   context "test word is a palindrome" do
     it "should read the same forwards as backwards" do
       test_word = "Madam"
-      result = Word.palindrome?(test_word.downcase)
+      result = Word.palindrome?(test_word)
       expect(result).to be_truthy
     end
   end
@@ -12,7 +12,7 @@ describe Word do
   context "test word is not a palindrome" do
     it "should not read the same forwards as backwards" do
       test_word = "Food"
-      result = Word.palindrome?(test_word.downcase)
+      result = Word.palindrome?(test_word)
       expect(result).to be_falsey
     end
   end
