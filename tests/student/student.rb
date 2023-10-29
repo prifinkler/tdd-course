603cbcc3629b2688f4c66201e437ba7a16f3d363
Priscila Finkler <prifinkler@gmail.com>
2023-10-29 13:44:15 +0000

    refactor code and use additional tools

--- tests/student/student.rb
+++ tests/student/student.rb
@@ -1,12 +1,18 @@
 class Student
   attr_reader :first_name, :last_name
+  @@total_count = 0
 
   def initialize(first, last)
+    @@total_count += 1
     @first_name = first
     @last_name = last
   end
 
   def student_fullname
     "#{first_name} #{last_name}"
+  end
+
+  def self.total_count
+    @@total_count
   end
 end
--- tests/student/student_spec.rb
+++ tests/student/student_spec.rb
@@ -1,20 +1,23 @@
 require_relative 'student'
 
 RSpec.describe Student do
+  describe "instance methods" do
 
-  before do
-    @student = Student.new("John", "Doe")
+    subject { Student.new("John", "Doe") }
+
+    it { respond_to(:first_name) }
+
+    it { respond_to(:last_name) }
+
+    it { respond_to(:student_fullname) }
   end
 
-  it "should respond to #first_name" do
-    expect(@student).to respond_to(:first_name)
-  end
+  describe "total number of students created" do
+    it "should have 2 students in total" do
+      Student.new("John", "Doe")
+      Student.new("Jane", "Doe")
 
-  it "should respond to #last_name" do
-    expect(@student).to respond_to(:last_name)
-  end
-
-  it "should respond to #student_fullname" do
-    expect(@student).to respond_to(:student_fullname)
+      expect(Student.total_count).to eq(2)
+    end
   end
 end
